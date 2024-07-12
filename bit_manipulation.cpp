3419907a1a29c5a13d2185c8afb6c39a4b4fe297
Adeel Javed <93484102+adeel-015@users.noreply.github.com>
2024-07-12 16:20:41 +0530

    Create bit_manipulation.cpp

---
+++ bit_manipulation.cpp
@@ -0,0 +1,40 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+//Decimal to Binary (Time and Space Complexity --> O(log n))
+vector<int> decbinary(int n){
+    vector<int> result;
+    while (n>0){
+        result.push_back(n % 2);
+        n/=2;
+    }
+    return result;
+}
+
+// Binary to Decimal (Time and Space Complexity --> O(log n))
+int bin(vector<int> n){
+    int ans = 0, pow2 = 1;
+    for (int i=0;i<n.size();i++){
+        ans += n[i] * pow2;
+        pow2 *= 2;
+    }
+    return ans;
+}
+
+int main() {
+    // Write C++ code here
+    vector<int> result = decbinary(5);
+    
+    for (int i : result){
+        cout << i << " ";
+    }
+    cout << endl;
+    
+    int d = bin(result);
+    cout << d;
+
+    return 0;
+}
